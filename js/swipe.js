971276a61aecaaf43f71498d29e6f09b38c180a8
MarxJiao <610529111@qq.com>
2016-08-28 04:17:54 +0800

    add

--- js/swipe.js
+++ js/swipe.js
@@ -4,33 +4,40 @@ function a() {
     var ham = new Hammer.Manager(el);
     ham.add(new Hammer.Pan({ threshold: 10 }));
     ham.add(new Hammer.Pinch());
-    ham.on('pan', onPan);
+    ham.on('pan panend', onPan);
     ham.on('pinch', onPinch);
     var transX = 0;
     var index = 0;
 
     function onPan(ev) {
-        
+        el.childNodes[1].className = '';
         if (ev.type == 'panend') {
             console.log(transX);
             if (ev.deltaX > 10) {
-                if (index == 2) {
-                    index +=1;
-                }
-            }
-            if (ev.deltaX < -10) {
                 if (index != 0){
                     index -=1;
                 }
+                
             }
-            el.childNodes[1].style.transform = 'translate('+index*340+'px, 0px)';
+            if (ev.deltaX < -10) {
+                if (index != 2) {
+                    index +=1;
+                }
+            }
+            console.log(index)
+            el.childNodes[1].className = 'animation';
+            el.childNodes[1].style.transform = 'translate('+index*(-340)+'px, 0px)';
             transX = 0;
         }
-        // console.log(ev);
-        console.log(ev);
+        else {
+            // console.log(ev);
+            console.log(ev);
+            
+            el.childNodes[1].style.transform = 'translate('+ev.deltaX+'px, 0px)';
+            transX = ev.deltaX;
+
+        }
         
-        el.childNodes[1].style.transform = 'translate('+ev.deltaX+'px, 0px)';
-        transX = ev.deltaX;
     };
     var initScale = 1;
     function onPinch(ev) {
