f85bbe6da5c40ab5f5e8c1f07e0822a556599392
MarxJiao <610529111@qq.com>
2016-08-28 04:05:02 +0800

    add

--- js/swipe.js
+++ js/swipe.js
@@ -4,32 +4,33 @@ function a() {
     var ham = new Hammer.Manager(el);
     ham.add(new Hammer.Pan({ threshold: 10 }));
     ham.add(new Hammer.Pinch());
-    ham.on('panstart panmove', onPan);
+    ham.on('pan', onPan);
     ham.on('pinch', onPinch);
     var transX = 0;
     var index = 0;
 
     function onPan(ev) {
         
-        if (ev.type = 'panend') {
+        if (ev.type == 'panend') {
             console.log(transX);
-            if (transX > 10) {
+            if (ev.deltaX > 10) {
                 if (index == 2) {
                     index +=1;
                 }
             }
-            if (transX < -10) {
+            if (ev.deltaX < -10) {
                 if (index != 0){
                     index -=1;
                 }
             }
-            // el.childNodes[1].style.transform = 'translate('+index*340+'px, 0px)';
-            // transX = 0;
+            el.childNodes[1].style.transform = 'translate('+index*340+'px, 0px)';
+            transX = 0;
         }
         // console.log(ev);
         console.log(ev);
-        transX += ev.deltaX/2;
-        el.childNodes[1].style.transform = 'translate('+transX+'px, 0px)';
+        
+        el.childNodes[1].style.transform = 'translate('+ev.deltaX+'px, 0px)';
+        transX = ev.deltaX;
     };
     var initScale = 1;
     function onPinch(ev) {
