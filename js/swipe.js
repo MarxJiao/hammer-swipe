7361f458d5f61a59a64bc0ca075c45770a498923
MarxJiao <610529111@qq.com>
2016-08-28 02:28:37 +0800

    add

--- js/swipe.js
+++ js/swipe.js
@@ -1,9 +1,17 @@
-window.onload = function () {
+function a() {
     var el = document.getElementById('iSlider');
+    console.log(el.childNodes[1]);
     var ham = new Hammer.Manager(el);
     ham.add(new Hammer.Pan({ threshold: 0 }));
-    ham.on('pan', onPan);
-    function onPan() {
-        
+    ham.on('pan panend', onPan);
+    var transX = 0;
+    function onPan(ev) {
+        if (ev.type = 'panend') {
+            console.log(transX);
+        }
+        // console.log(ev);
+        console.log(ev.velocityX);
+        transX += ev.velocityX;
+        el.childNodes[1].style.transform = 'translate('+transX*10+'px, 0px)';
     }
 }
